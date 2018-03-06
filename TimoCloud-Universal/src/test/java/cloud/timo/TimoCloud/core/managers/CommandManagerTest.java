2d5222234c773b196bd61649200f74de92a7a2fe
TimoCrafter <timo.zikeli@gmail.com>
2018-03-06 20:57:13 +0100

    Add first test

--- TimoCloud-Universal/src/test/java/cloud/timo/TimoCloud/core/managers/CommandManagerTest.java
+++ TimoCloud-Universal/src/test/java/cloud/timo/TimoCloud/core/managers/CommandManagerTest.java
@@ -1,12 +1,45 @@
 package cloud.timo.TimoCloud.core.managers;
 
+import cloud.timo.TimoCloud.core.TimoCloudCore;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.*;
+import java.util.function.Consumer;
 
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TimoCloudCore.class)
 public class CommandManagerTest {
 
+    @Mock
+    private TimoCloudCore timoCloudCore;
+    @Mock
+    private CoreServerManager serverManager;
+
+    private CommandManager commandManager;
+
+    @Before
+    public void setUp() {
+        commandManager = new CommandManager();
+        PowerMockito.mockStatic(TimoCloudCore.class);
+        when(TimoCloudCore.getInstance()).thenReturn(timoCloudCore);
+        when(timoCloudCore.getServerManager()).thenReturn(serverManager);
+    }
+
     @Test
-    public void onCommand() {
+    public void onCommandReload() {
+        CoreFileManager fileManager = mock(CoreFileManager.class);
+        when(timoCloudCore.getFileManager()).thenReturn(fileManager);
+        Consumer<String> sendMessage = mock(Consumer.class);
+        commandManager.onCommand(sendMessage, true, "reload");
+        verify(fileManager, times(1)).load();
+        verify(serverManager, times(1)).loadGroups();
+        verify(sendMessage, times(1)).accept(anyString());
     }
 }
