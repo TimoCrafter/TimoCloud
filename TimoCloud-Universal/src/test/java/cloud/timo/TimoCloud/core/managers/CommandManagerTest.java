6198746e2057512973010564ac6403b38a4f6f02
TimoCrafter <timo.zikeli@gmail.com>
2018-03-06 21:02:57 +0100

    Comment first test

--- TimoCloud-Universal/src/test/java/cloud/timo/TimoCloud/core/managers/CommandManagerTest.java
+++ TimoCloud-Universal/src/test/java/cloud/timo/TimoCloud/core/managers/CommandManagerTest.java
@@ -13,33 +13,33 @@ import java.util.function.Consumer;
 
 import static org.mockito.Mockito.*;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(TimoCloudCore.class)
+@RunWith(PowerMockRunner.class) // We needed that for every test, in order to mock static methods
+@PrepareForTest(TimoCloudCore.class) // We want to mock TimoCloudCore statically
 public class CommandManagerTest {
 
     @Mock
-    private TimoCloudCore timoCloudCore;
+    private TimoCloudCore timoCloudCore; // Our TimoCloudCore mock
     @Mock
-    private CoreServerManager serverManager;
+    private CoreServerManager serverManager; // Our ServerManager mock
 
-    private CommandManager commandManager;
+    private CommandManager commandManager; // Our CommandManager instance we want to test
 
     @Before
     public void setUp() {
-        commandManager = new CommandManager();
-        PowerMockito.mockStatic(TimoCloudCore.class);
-        when(TimoCloudCore.getInstance()).thenReturn(timoCloudCore);
-        when(timoCloudCore.getServerManager()).thenReturn(serverManager);
+        commandManager = new CommandManager(); // Create instance
+        PowerMockito.mockStatic(TimoCloudCore.class); // Mock TimoCloudCore statically
+        when(TimoCloudCore.getInstance()).thenReturn(timoCloudCore); // Return our mocked instance
+        when(timoCloudCore.getServerManager()).thenReturn(serverManager); // Return mocked ServerManager
     }
 
     @Test
-    public void onCommandReload() {
-        CoreFileManager fileManager = mock(CoreFileManager.class);
-        when(timoCloudCore.getFileManager()).thenReturn(fileManager);
-        Consumer<String> sendMessage = mock(Consumer.class);
-        commandManager.onCommand(sendMessage, true, "reload");
-        verify(fileManager, times(1)).load();
-        verify(serverManager, times(1)).loadGroups();
-        verify(sendMessage, times(1)).accept(anyString());
+    public void onCommandReload() { // Test "reload" command
+        CoreFileManager fileManager = mock(CoreFileManager.class); // Mock FileManager
+        when(timoCloudCore.getFileManager()).thenReturn(fileManager); // Return FileManager mock in TimoCloudCore mock
+        Consumer<String> sendMessage = mock(Consumer.class); // Mocked Consumer to check if messages have been sent
+        commandManager.onCommand(sendMessage, true, "reload"); // Run the method we want to test
+        verify(fileManager, times(1)).load(); // Check if 'load' method has been called in FileManager
+        verify(serverManager, times(1)).loadGroups(); // Check if 'loadGroups' has been called in ServerManager
+        verify(sendMessage, times(1)).accept(anyString()); // Check if message has been sent to user
     }
 }
