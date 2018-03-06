0cfa1365868b4646307167b90fd44881e0b4dcc8
TimoCrafter <timo.zikeli@gmail.com>
2018-03-06 19:51:51 +0100

    Introcuce Test with JUnit 4

---
+++ TimoCloud-Universal/src/test/java/cloud/timo/TimoCloud/core/managers/CommandManagerTest.java
@@ -0,0 +1,12 @@
+package cloud.timo.TimoCloud.core.managers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommandManagerTest {
+
+    @Test
+    public void onCommand() {
+    }
+}
